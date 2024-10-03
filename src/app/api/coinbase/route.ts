2ee47a97ca90c3e4cf3f705425db8581a44c18ac
computerdev21 <dev007chetal@gmail.com>
2024-10-03 04:56:33 -0400

    Minor change

--- src/app/api/coinbase/route.ts
+++ src/app/api/coinbase/route.ts
@@ -6,7 +6,7 @@ export async function GET(req: NextRequest) {
     const clientId = process.env.NEXT_PUBLIC_COINBASE_CLIENT_ID;
     const redirectUri = process.env.NEXT_PUBLIC_COINBASE_REDIRECT_URI;
     const scope = 'wallet:accounts:read,wallet:transactions:read';
-    console.log(clientId);
+    console.log(redirectUri);
 
     const authorizationUrl = `https://www.coinbase.com/oauth/authorize?response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scope}`;
 
