092df856e421729d45dd2cc4268b82bbc55d5382
computerdev21 <dev007chetal@gmail.com>
2024-10-03 05:14:17 -0400

    Error fix

--- src/app/api/coinbase/route.ts
+++ src/app/api/coinbase/route.ts
@@ -1,6 +1,20 @@
 import { NextRequest, NextResponse } from 'next/server';
-import axios, { AxiosError } from 'axios';
+import axios, {AxiosError} from 'axios';
 
+// GET /api/coinbase/connect
+export async function GET(req: NextRequest) {
+    const clientId = process.env.NEXT_PUBLIC_COINBASE_CLIENT_ID;
+    const redirectUri = process.env.NEXT_PUBLIC_COINBASE_REDIRECT_URI;
+    const scope = 'wallet:accounts:read,wallet:transactions:read';
+    console.log(redirectUri);
+
+    const authorizationUrl = `https://www.coinbase.com/oauth/authorize?response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scope}`;
+
+    // Respond with the authorization URL
+    return NextResponse.json({ authorizationUrl });
+}
+
+// POST /api/coinbase/callback
 export async function POST(req: NextRequest) {
     const { searchParams } = new URL(req.url);
     const code = searchParams.get('code'); // Get the authorization code from the query
